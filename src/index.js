c77f98f6695dbf4634184554f55d21637ff6a735
XDshnik <pesherovpavel@gmail.com>
2021-03-13 14:06:31 +0300

    feat: add brackets functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if (str.includes(bracketsConfig[j][0] + bracketsConfig[j][1])) {
+        str = str.replace(bracketsConfig[j][0] + bracketsConfig[j][1], "");
+        i--;
+      }
+    }
+  }
+  let result = (str.length != 0) ? false : true;
+  return result
 }
